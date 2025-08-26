e55ef4264100a1b9ae5f1fe1c6016215da9f3cc8
Ella <ellatal2090@gmail.com>
2025-08-26 15:40:22 +0300

    week1/day1: completed Exercise 1(Hello World)

---
+++ week1/day1/exercises_xp/basic-data-types.py
@@ -0,0 +1,17 @@
+#Exercise 1 : Hello World
+# Instructions
+# Print the following output in one line of code:
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+#Dublicating the line(shift + option + down arrow)
+
+print('Hello World\nHello World\nHello World\nHello World')
+#Using \n - newline
+
