69242c0ca6c5e57a090f07feb899d4a8d2e7e28c
Ella <ellatal2090@gmail.com>
2025-08-26 16:05:26 +0300

    week1/day1: completed Ex 2(Some Math)

--- week1/day1/exercises_xp/basic-data-types.py
+++ week1/day1/exercises_xp/basic-data-types.py
@@ -15,3 +15,14 @@ print("Hello World")
 print('Hello World\nHello World\nHello World\nHello World')
 #Using \n - newline
 
+# Exercise 2 : Some Math
+# Instructions
+# Write code that calculates the result of:
+# (99^3)*8 (meaning 99 to the power of 3, times 8).
+
+result = (99**3)* 8
+print(result)
+#at first I used (:) and it gave me - "NameError: name 'result' is not defined"
+#then I replased ":" and "="
+
+
