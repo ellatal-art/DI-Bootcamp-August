2f775813ec822d49e2a3de939cc097384df24ed6
Ella <ellatal2090@gmail.com>
2025-08-29 02:24:54 +0300

    week1/day2 Completed Exercise 3: List Manipulation

--- week1/day2/exercises_xp/basic-data-types.py
+++ week1/day2/exercises_xp/basic-data-types.py
@@ -45,6 +45,30 @@ temp_list.append(5)
 my_tuple = tuple(temp_list)
 print(my_tuple)
 
+    #Exercise 3: List Manipulation
+# Key Python Topics:
+# Lists
+# List methods: append, remove, insert, count, clear
+    # Instructions:
+# You have a list: basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# Remove "Banana" from the list.
+# Remove "Blueberries" from the list.
+# Add "Kiwi" to the end of the list.
+# Add "Apples" to the beginning of the list.
+# Count how many times "Apples" appear in the list.
+# Empty the list.
+# Print the final state of the list.
 
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+basket.remove('Blueberries')
+basket.append("Kiwi")
+basket.sort()
+print(basket)
+basket.count("Apples")
+print(basket.count('Apples'))
+basket.clear()
+print(basket)
+    # Everything here is pretty logical: the methods do exactly what their names say
 
     
